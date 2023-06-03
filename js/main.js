2d0a0a1580d05e7da0d772c6c800b108c3fe313e
767sofia <sofia.aguilar@itscordoba.edu.ar>
2023-06-02 21:21:52 -0300

    creo archivo js

---
+++ js/main.js
@@ -0,0 +1,61 @@
+//Variable de tipo numerica
+let numero = 5;
+
+//numero de caracteres con length
+let frase = "Sofia"
+let longitud = frase.length
+
+//Variable boleana
+let a = false;
+let b = true;
+
+//Array
+const miArray = ["Sofia aguilar",23, "Cordoba"];
+//console.log(miArray[0]);
+//console.log(miArray[2]);
+
+//Variable objeto 
+const objeto = {
+    nombre :"sofia aguilar",
+    edad : 23,
+    estatura : 1.60
+}
+//console.log(objeto.edad);
+
+//operaciones aritmética
+let suma = 5 + 5 ;
+let multiplicacion = 5 * 5 ;
+let division = 10 / 5 ;
+let resta = 10 - 5;
+// console.log("Esto es una suma: " + suma ,
+// "Esto es una multiplicacion: " + multiplicacion, 
+// "Esto es una division: " + division ,
+// "Esto es una resta: " + resta);
+
+//Combinacion de dos cadenas de texto
+let frase1 = "Hola ";
+let frase2 = "como estas";
+let combinacion = frase1 + frase2
+
+//Comparacion con ==
+let comparacion1 = "Hola";
+let comparacion2 = "como estas";
+//console.log(comparacion1 == comparacion2);
+
+//Operadores lógicos 
+
+//NOT!
+let valor = true
+console.log(!valor)
+
+//OR
+
+//AND
+
+let valor1= 1;
+let valor2 = 2;
+let valor3 = 5;
+let valor4= 10;
+let comp = (valor1 < valor2) && (valor3 < valor4);
+
+console.log(comp);
